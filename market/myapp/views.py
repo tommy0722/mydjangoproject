a0a28d9640addc247255b303eaaa93dbb4fb8e33
tommy <t40201@gmail.com>
2023-02-02 13:43:26 +0800

    將資料傳到前端

--- market/myapp/views.py
+++ market/myapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from myapp.models import ProductModel
 # Create your views here.
 def index(request):
+    products=ProductModel.objects.all()
     return render(request,'index.html',locals())
