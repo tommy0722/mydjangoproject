2521e93e8a0f7518046c68c79976e07951d72032
tommy <t40201@gmail.com>
2023-02-06 22:58:01 +0800

    後臺排版、購物車email判斷

--- market/myapp/admin.py
+++ market/myapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ProductModel,OrderModel,DetailModel
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display = ('pname', 'pprice', 'pimage','pdescription')
+class OrderModelAdmin(admin.ModelAdmin):
+    list_display = ('id','subtotal', 'shipping', 'grandtotal','customername'
+    ,'customerphone','customeremail','customeraddress','paytype')
+    search_fields = ('customername',)
+class DetailModelAdmin(admin.ModelAdmin):
+    list_display = ('id','dorder', 'pname', 'unitprice','quantity','dtotal')
+admin.site.register(ProductModel,ProductModelAdmin)
+admin.site.register(OrderModel,OrderModelAdmin)
+admin.site.register(DetailModel,DetailModelAdmin)
